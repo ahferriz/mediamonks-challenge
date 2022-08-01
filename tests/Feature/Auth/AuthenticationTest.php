d5045e901f91ced23c84220f793349c3e10309a5
ahferriz <ahferriz@gmail.com>
2022-07-31 19:39:35 -0500

    Test debug

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -14,8 +14,9 @@ class AuthenticationTest extends TestCase
     public function test_login_screen_can_be_rendered()
     {
         $response = $this->get('/login');
-
-        $response->assertStatus(200);
+print_r($response);
+        //$response->assertStatus(200);
+        $response->assertSeeText("Example");
     }
 
     public function test_users_can_authenticate_using_the_login_screen()
